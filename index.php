0fe43da81745cc70a2a86ba7613e1de9f8190485
fdimorais <fdimorais@hotmail.com>
2019-05-28 19:19:37 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+
+<!-- MONITOR REDE 
+	 ADICIONAR
+	 DELETAR
+	 ALTERAR
+	 SELECT
+-->
 <link rel="stylesheet" href="css/estilo.css">
 <!-- comentário -->
 <title>Monitor de Redes</title>
+<?php require_once 'action/db_connect.php'; ?>
 </head>
 <body>
 <div class="container">
@@ -40,8 +48,6 @@ OFFLINE
 </div>
 </div>
 </div>
-
-
 
 </div>
 
